963a99e7e035dd515ace7300e39eb527ac19cd8b
Ajay V P <vpajay.99@gmail.com>
2024-12-22 00:28:05 +0530

    changes in code for handling index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppLayout from "./src/App";
+
+// Mount the App to the #root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
